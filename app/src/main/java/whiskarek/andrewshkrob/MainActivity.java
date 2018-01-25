489bad9539590b1ed450a9461f9692c999549e90
Andrew Shkrob <andrei5709978@gmail.com>
2018-01-25 22:23:27 +0300

    Integrated CheckStyle

--- app/src/main/java/whiskarek/andrewshkrob/MainActivity.java
+++ app/src/main/java/whiskarek/andrewshkrob/MainActivity.java
@@ -1,27 +1,21 @@
 package whiskarek.andrewshkrob;
 
+import static android.net.Uri.parse;
+
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
-
-import java.util.Random;
-
-import static android.net.Uri.parse;
 
 public class MainActivity extends AppCompatActivity {
 
